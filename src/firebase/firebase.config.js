888639199f4856705a2dd7be1a2b54f79e5fbab4
bilkish-akther <bilkishakther921@gmaile.com>
2023-07-02 07:24:00 -0700

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,38 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +40,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyABGLmPe2y0_kpolwdXBRj4_fHmf2agi8s",
+  authDomain: "mastery-karate.firebaseapp.com",
+  projectId: "mastery-karate",
+  storageBucket: "mastery-karate.appspot.com",
+  messagingSenderId: "614211861145",
+  appId: "1:614211861145:web:85265449834663ad1a5608"
 };
 
 // Initialize Firebase
